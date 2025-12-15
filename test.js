7bbf74eaa2da2faa32f7b17a9e5c9290bbd03a92
kapilansh-alchemystai <kapilansh@getalchemystai.com>
2025-12-15 14:54:10 +0530

    add eslint and lint step

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ console.log("Running tests...");
 console.log("All tests passed!");
 
 // throw new Error("Test failed intentionally");
+console.log("Linting code");
+console.log("Lint passed");
+
+unusedVar = 42;
+console.log("Running tests...");
